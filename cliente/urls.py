836d9818962d92fd8d7deabd038366e08f5f496e
luciano <ribeirovianaluciano98@gmail.com>
2024-10-11 18:25:26 -0300

    Adção de dados no banco de dados

--- cliente/urls.py
+++ cliente/urls.py
@@ -3,5 +3,8 @@ from.import views
 
 urlpatterns = [
     path('cliente/', views.cliente, name='cliente'),
-    path('formulario/', views.formulario, name='formulario')
+    path('formulario/', views.formulario, name='formulario'),
+    path('create/', views.create, name='create'),
+    path('view/<int:pk>/', views.view,name="view")
+    
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from cliente.forms import ClienteForm
+from cliente.models import Cliente
 
 # Create your views here.
 def cliente(request):
-    return render(request,'index.html')
+    data ={}
+    data['db'] = Cliente.objects.all()
+    return render(request,'index.html', data)
 
 def formulario(request):
     data ={}
     data['form'] = ClienteForm()
     return render(request,'form.html' ,data)
+
+def create(request):
+    form = ClienteForm(request.POST or None)#recebe o resultado do post do formulario.
+    if form. is_valid():#verificar se esta tudo certinho no formulario
+        form.save()#Salvarno banco de dados
+        return redirect('cliente')#retornar par pagina inicial
+    
+def view(request, pk):
+    data = {}
+    data['db'] = Cliente.objects.get(pk=pk)
+    return render(request, 'view.html', data)    
+        
+    
